8e8b66ee3c8a13c5c4ec6b43ea2b5623db6ccb27
romanivanov91 <romanivanov91@yandex.ru>
2022-12-07 19:28:19 +0300

    Procedures_1

---
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -0,0 +1,12 @@
+--Создаем процедуру dbo.usp_MakeFamilyPurchase
+create proc dbo.usp_MakeFamilyPurchase
+@FamilySurName as varchar(255)
+as
+if (@FamilySurName in (select FamilySurName
+						from dbo.Family))
+	update dbo.Family
+	set BudgetValue = BudgetValue - (select sum(Value)
+									from dbo.Basket)
+	where FamilySurName = @FamilySurName
+	else
+	print 'Такой семьи нет!!!'
