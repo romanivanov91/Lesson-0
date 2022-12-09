15483232d604e166746feb425a6dd0267e9a2a0c
romanivanov91 <romanivanov91@yandex.ru>
2022-12-09 18:54:55 +0300

    Triggers_3

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -5,9 +5,9 @@ as
 update dbo.Basket
 set DiscountValue = case
 						when ID_SKU in (select ID_SKU
-						from inserted
-						group by ID_SKU
-						having count(ID_SKU) >= 2)
+										from inserted
+										group by ID_SKU
+										having count(ID_SKU) >= 2)
 						then Value * 0.05
 						else 0
 						end
