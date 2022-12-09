f75e32bfcd0ab956325820237fbd8b2e902ede90
romanivanov91 <romanivanov91@yandex.ru>
2022-12-09 18:44:15 +0300

    Triggers_2

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -3,19 +3,13 @@ create trigger dbo.TR_Basket_insert_update on dbo.Basket after insert
 as
 
 update dbo.Basket
-set DiscountValue = Value * 0.05
-where ID in (select ID
-			from inserted)
-		and ID_SKU in (select ID_SKU
+set DiscountValue = case
+						when ID_SKU in (select ID_SKU
 						from inserted
 						group by ID_SKU
 						having count(ID_SKU) >= 2)
-
-update dbo.Basket
-set DiscountValue = 0
+						then Value * 0.05
+						else 0
+						end
 where ID in (select ID
 			from inserted)
-		and ID_SKU in (select ID_SKU
-						from inserted
-						group by ID_SKU
-						having count(ID_SKU) = 1)
