b39d9e3a9b1217bcf8c907366673293e42a41bac
romanivanov91 <romanivanov91@yandex.ru>
2022-12-09 18:17:36 +0300

    VEWs_4

--- dbo.vw_SKUPriceв.sql
+++ dbo.vw_SKUPriceв.sql
@@ -1,7 +1,4 @@
 --Создаем представления dbo.vw_SKUPriceв (view)
 create view dbo.vw_SKUPriceв as
-select s.Code, s.Name, dbo.udf_GetSKUPrice(ID_SKU) as cost
-from dbo.SKU as s
-	join
-	dbo.Basket as b
-	on s.ID = b.ID_SKU
+select Code, Name, dbo.udf_GetSKUPrice(ID) as cost
+from dbo.SKU
